a975c5597e5bd8af8c365294c1548d4b19ff4134
Fabio Sanna <rrabio@rrabiocar>
2023-11-24 18:37:40 +0100

    asdasd1

--- main.tf
+++ main.tf
@@ -23,14 +23,14 @@ provider "azurerm" {
 }
 
 module "azurerm-secstorage2" {
-  source  = "app.terraform.io/rrabionet/azurerm-secstorage2/terraform"
-  version = "1.0.1"
-  location = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  source               = "app.terraform.io/rrabionet/azurerm-secstorage2/terraform"
+  version              = "1.0.1"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
   storage_account_name = "porcodio12345"
 }
- 
+
 resource "azurerm_resource_group" "rg" {
-  name = "813-4551346f-hands-on-with-terraform-on-azure"
+  name     = "813-4551346f-hands-on-with-terraform-on-azure"
   location = "southcentralus"
 }
