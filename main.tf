e215215cf16b4ccd156aa10668fdd4a794032dd2
Fabio Sanna <rrabio@rrabiocar>
2023-11-24 17:04:15 +0100

    asd

--- main.tf
+++ main.tf
@@ -26,3 +26,26 @@ module "azurerm-secstorage2" {
   source  = "app.terraform.io/rrabionet/azurerm-secstorage2/terraform"
   version = "1.0.0"
 }
+
+
+resource "azurerm_resource_group" "rg" {
+  location = "eastus"
+  name     = "813-eca3f4f8-hands-on-with-terraform-on-azure"
+}
+
+
+variable "ARM_CLIENT_ID" {
+
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+
+}
+
+variable "ARM_CLIENT_SECRET" {
+
+}
+
+variable "ARM_TENANT_ID" {
+
+}
