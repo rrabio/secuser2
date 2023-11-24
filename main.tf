e730a451e62fae3f13f86bdb67b287c69659e490
Fabio Sanna <rrabio@rrabiocar>
2023-11-24 16:51:11 +0100

    stoca

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">=1.3.0"
+  required_providers {
+    azurerm = {
+      "source" = "hashicorp/azurerm"
+      version = ">= 3.43.0"
+    }
+  }
+
+  cloud {
+    organization = "rrabionet"
+
+    workspaces {
+      name = "Terraform_CI"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+module "azurerm-secstorage2" {
+  source  = "app.terraform.io/rrabionet/azurerm-secstorage2/terraform"
+  version = "1.0.0"
+}
